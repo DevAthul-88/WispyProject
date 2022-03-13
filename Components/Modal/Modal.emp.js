529069783edb8ee4e4cd09d4c1a17071b8ca8b6e
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-13 15:05:30 +0530

    modal

--- Components/Modal/Modal.emp.js
+++ Components/Modal/Modal.emp.js
@@ -1,39 +1,152 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Button,
-  } from '@chakra-ui/react'
-import { useState } from 'react'
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+import { Formik, Form, Field } from "formik";
+import SignupSchema from "../../Validation/signup";
+import { useDispatch, useSelector } from "react-redux";
+import Alert from "../../Components/Alert";
 
+function Model({ toggle, setToggle }) {
+  return (
+    <>
+      <Modal isOpen={toggle} size={"xl"}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Add employees</ModalHeader>
+          <ModalBody>
+            <Flex justify={"center"}>
+              <Formik
+                initialValues={{
+                  username: "",
+                  email: "",
+                  org: "",
+                  password: "",
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={(values, { resetForm }) => {
+                  console.log(values);
+                }}
+              >
+                {({ errors, touched }) => (
+                  <Stack spacing={4} w={"full"} maxW={"md"}>
+                    <Form>
+                      <FormControl marginTop={"5"}>
+                        <FormLabel>Username</FormLabel>
+                        <Field
+                          name="username"
+                          as={CustomInputComponent}
+                          type={"text"}
+                          focusBorderColor={"messenger.500"}
+                          borderColor={
+                            errors.username && touched.username
+                              ? "red.500"
+                              : "gray.300"
+                          }
+                        />
+                        {errors.username && touched.username ? (
+                          <FormLabel color={"red.600"}>
+                            {errors.username}
+                          </FormLabel>
+                        ) : (
+                          ""
+                        )}
+                      </FormControl>
 
-function Model({toggle , setToggle}) {
+                      <FormControl id="email" marginTop={"5"}>
+                        <FormLabel>Email address</FormLabel>
+                        <Field
+                          name="email"
+                          as={CustomInputComponent}
+                          type={"email"}
+                          focusBorderColor={"messenger.500"}
+                          borderColor={
+                            errors.email && touched.email
+                              ? "red.500"
+                              : "gray.300"
+                          }
+                        />
+                        {errors.email && touched.email ? (
+                          <FormLabel color={"red.600"}>
+                            {errors.email}
+                          </FormLabel>
+                        ) : (
+                          ""
+                        )}
+                      </FormControl>
 
-    return (
-      <>
-  
-        <Modal isOpen={toggle}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalBody>
-              
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={() => setToggle(!toggle)}>
-                Close
-              </Button>
-              <Button variant='ghost'>Secondary Action</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+                      <FormControl marginTop={"5"}>
+                        <FormLabel>Your organization name</FormLabel>
+                        <Field
+                          name="org"
+                          as={CustomInputComponent}
+                          type={"text"}
+                          focusBorderColor={"messenger.500"}
+                          borderColor={
+                            errors.org && touched.org ? "red.500" : "gray.300"
+                          }
+                        />
+                        {errors.org && touched.org ? (
+                          <FormLabel color={"red.600"}>{errors.org}</FormLabel>
+                        ) : (
+                          ""
+                        )}
+                      </FormControl>
+                      <FormControl marginTop={"5"}>
+                        <FormLabel>Password</FormLabel>
+                        <Field
+                          name="password"
+                          as={CustomInputComponent}
+                          type={"password"}
+                          focusBorderColor={"messenger.500"}
+                          borderColor={
+                            errors.password && touched.password
+                              ? "red.500"
+                              : "gray.300"
+                          }
+                        />
+                        {errors.password && touched.password ? (
+                          <FormLabel color={"red.600"}>
+                            {errors.password}
+                          </FormLabel>
+                        ) : (
+                          ""
+                        )}
+                      </FormControl>
+                    </Form>
+                    <ModalFooter>
+                      <Button
+                        colorScheme="red"
+                        mr={3}
+                        onClick={() => setToggle(!toggle)}
+                      >
+                        Close
+                      </Button>
+                      <Button colorScheme={"messenger"} type="submit">Save</Button>
+                    </ModalFooter>
+                  </Stack>
+                )}
+              </Formik>
+            </Flex>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
-  export default Model
+export default Model;
+
+const CustomInputComponent = (props) => (
+  <Input type={props.type} {...props} width={"full"} />
+);
