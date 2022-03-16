c723b9189e3b0664f78835ff7d0f560b4d94c039
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-16 12:03:36 +0530

    changes

---
+++ Components/project.details.js
@@ -0,0 +1,130 @@
+import {
+    Box,
+    chakra,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    VisuallyHidden,
+    List,
+    ListItem,
+  } from '@chakra-ui/react';
+  import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+  import { MdLocalShipping } from 'react-icons/md';
+  
+  export default function Details({data}) {
+    return (
+      <Container maxW={'7xl'}>
+        <SimpleGrid
+         
+          py={{ base: 18, md: 4 }}>
+         
+          <Stack  divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.200', 'gray.600')}
+                />
+              }>
+            <Box as={'header'}>
+            <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={"messenger.500"}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}>
+                  Title
+                </Text>
+                <Text color={useColorModeValue('gray.900', 'gray.400')}
+              fontWeight={300}
+              fontSize={'2xl'} textTransform={"capitalize"}>{data[0].title}</Text>
+              
+            </Box>
+  
+            <Stack
+            
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.200', 'gray.600')}
+                />
+              }>
+           
+              <Box marginTop={"5"}>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={"messenger.500"}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}>
+                  Description
+                </Text>
+  
+               <Text color={useColorModeValue('gray.900', 'gray.400')}
+              fontWeight={300}
+              fontSize={'2xl'} textTransform={"capitalize"}>{data[0].description}</Text>
+              </Box>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={"messenger.500"}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}>
+                  Details
+                </Text>
+  
+                <List spacing={2}>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Priority:
+                    </Text>{' '}
+                     {data[0].priority}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Total members:
+                    </Text>{' '}
+                     {data[0].members.length}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Created At:
+                    </Text>{' '}
+                     {data[0].createdAt}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      updated At:
+                    </Text>{' '}
+                    {data[0].updatedAt}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Flagged as completed:
+                    </Text>{' '}
+                     {data[0].completed.flagged}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Completed:
+                    </Text>{' '}
+                    {data[0].completed.approved}
+                  </ListItem>
+               
+                </List>
+              </Box>
+            </Stack>
+  
+          
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    );
+  }
--- pages/software/projects/details/[slug].js
+++ pages/software/projects/details/[slug].js
@@ -1,10 +1,67 @@
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../../../../Components/Loader";
+import { useEffect } from "react";
+import { fetchData } from "../../../../redux/org/action";
+import {
+  Heading,
+  Container,
+  Text,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import Details from '../../../../Components/project.details'
 
 function Slug() {
-    const router = useRouter()
+  const dispatch = useDispatch();
+  const { data, loading } = useSelector((state) => state.org);
+  const { userInfo } = useSelector((state) => state.auth);
+  const router = useRouter();
+  const proj = data && data.projects.filter((e) => e.id == router.query.slug);
+  useEffect(() => {
+    dispatch(fetchData(userInfo._id));
+  }, []);
   return (
-    <div>{router.query.slug}</div>
-  )
+    <div>
+      <Head>
+        <title>WispyProject - Project-Details</title>
+      </Head>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {proj == undefined || null ? (
+            <Loader />
+          ) : (
+            <Container maxWidth={"container.lg"} marginRight={"20"}>
+              <Tabs isFitted variant="soft-rounded" colorScheme={"messenger"}>
+                <TabList mb="1em">
+                  <Tab>Details</Tab>
+                  <Tab>Comments</Tab>
+                  <Tab>Todos</Tab>
+                  <Tab>Attachments</Tab>
+                  <Tab>Users</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                   <Details data={proj}/>
+                  </TabPanel>
+                  <TabPanel>
+                    <p>two!</p>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+              
+            </Container>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Slug
+export default Slug;
