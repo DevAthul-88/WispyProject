2132c63c48cf9dd5d6b9793c1b6dc027a81944d7
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-20 11:14:32 +0530

    users table pagination

--- Components/Table/users.js
+++ Components/Table/users.js
@@ -1,19 +1,32 @@
 import React from "react";
-import { Table, Thead, Tbody, Tr, Th, Td, chakra , Link , Avatar} from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  chakra,
+  Link,
+  Avatar,
+  Flex,
+  Input,
+  Select,
+  Button
+} from "@chakra-ui/react";
 import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons";
-import { useTable, useSortBy} from "react-table";
-import NextLink from 'next/link'
+import { useTable, useSortBy , usePagination } from "react-table";
+import NextLink from "next/link";
 
 function DataTable({ org }) {
   const data = React.useMemo(() => org, []);
 
   const columns = React.useMemo(
     () => [
-      
       {
-         Header:"Avatar",
-         accessor: "av",
-         Cell:({row}) => (<Avatar name={row.original.username}/>)
+        Header: "Avatar",
+        accessor: "av",
+        Cell: ({ row }) => <Avatar name={row.original.username} />,
       },
       {
         Header: "username",
@@ -29,52 +42,124 @@ function DataTable({ org }) {
       },
       {
         Header: "View profile",
-        Cell: ({ row }) => (<>
-        <Link as={NextLink} href={`/software/profile/${row.original.id}`} >View profile</Link>
-        </> )
-      }
+        Cell: ({ row }) => (
+          <>
+            <Link as={NextLink} href={`/software/profile/${row.original.id}`}>
+              View profile
+            </Link>
+          </>
+        ),
+      },
     ],
     []
   );
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+    prepareRow,
+  } = useTable({ columns, data }, useSortBy, usePagination);
 
   return (
-    <Table {...getTableProps()}>
-      <Thead>
-        {headerGroups.map((headerGroup) => (
-          <Tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                {column.render("Header")}
-                <chakra.span pl="4">
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <TriangleDownIcon aria-label="sorted descending" />
-                    ) : (
-                      <TriangleUpIcon aria-label="sorted ascending" />
-                    )
-                  ) : null}
-                </chakra.span>
-              </Th>
+    <>
+    <Flex justify={"space-between"} marginTop="5">
+        <div></div>
+        <div className="pagination">
+          <Button
+            onClick={() => previousPage()}
+            disabled={!canPreviousPage}
+            colorScheme="messenger"
+          >
+            {"<"}
+          </Button>{" "}
+          <Button
+            onClick={() => nextPage()}
+            disabled={!canNextPage}
+            colorScheme="messenger"
+          >
+            {">"}
+          </Button>{" "}
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+          </span>
+          <span>
+            | Go to page:{" "}
+            <Input
+              focusBorderColor="messenger.500"
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </span>{" "}
+          <Select
+            marginTop={"4"}
+            focusBorderColor="messenger.500"
+            value={pageSize}
+            onChange={(e) => {
+              setPageSize(Number(e.target.value));
+            }}
+          >
+            {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
             ))}
-          </Tr>
-        ))}
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
+          </Select>
+        </div>
+      </Flex>
+      <Table {...getTableProps()}>
+        <Thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  <chakra.span pl="4">
+                    {column.isSorted ? (
+                      column.isSortedDesc ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : null}
+                  </chakra.span>
+                </Th>
               ))}
             </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+          ))}
+        </Thead>
+        <Tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <Td {...cell.getCellProps()}>{cell.render("Cell")}</Td>
+                ))}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </>
   );
 }
 
