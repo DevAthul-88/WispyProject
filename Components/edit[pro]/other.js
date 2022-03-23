088c04894cd0ae80eb389be4c44913767f4eb738
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-23 12:28:28 +0530

    changes

--- Components/edit[pro]/other.js
+++ Components/edit[pro]/other.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Heading, Button, Stack, Divider } from "@chakra-ui/react";
+import React from "react";
+import { useSelector } from "react-redux";
 
-function other({data , de , org}) {
+function other({ data, de, org }) {
+  const { userInfo } = useSelector((state) => state.auth);
   return (
-    <div>other</div>
-  )
+    <div>
+      {userInfo && (
+        <>
+          {de.completed.flagged ? (
+            <>
+              <Heading fontSize={"2xl"}>
+                This project is flagged as completed
+              </Heading>
+              {userInfo.role === "ADMIN" || userInfo.role === "PROJECT_MANAGER" ? (
+            <>
+              <Button colorScheme={"messenger"} marginTop={"5"} variant="outline">
+                Complete project
+              </Button>
+            </>
+          ) : null}
+            </>
+          ) : (
+            "This project is not completed yet"
+          )}
+          {userInfo.role === "ADMIN" || userInfo.role === "PROJECT_MANAGER" ? (
+            <>
+              <Divider marginTop={"5"} />
+              <Button colorScheme={"red"} marginTop={"5"} variant="outline">
+                Delete project
+              </Button>
+            </>
+          ) : null}
+        </>
+      )}
+    </div>
+  );
 }
 
-export default other
+export default other;
