d05c981da0a6941ad91d5ed21362f6d5dd8c6d2a
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-24 19:13:27 +0530

    changes

--- Components/edit[pro]/other.js
+++ Components/edit[pro]/other.js
@@ -1,9 +1,28 @@
-import { Heading, Button, Stack, Divider } from "@chakra-ui/react";
+import {
+  Heading,
+  Button,
+  Stack,
+  Divider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  useDisclosure,
+  ModalHeader,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
 
 function other({ data, de, org }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { userInfo } = useSelector((state) => state.auth);
+
+  const handleFinish = () => {
+
+  }
+
   return (
     <div>
       {userInfo && (
@@ -13,13 +32,19 @@ function other({ data, de, org }) {
               <Heading fontSize={"xl"}>
                 This project is flagged as completed
               </Heading>
-              {userInfo.role === "ADMIN" || userInfo.role === "PROJECT_MANAGER" ? (
-            <>
-              <Button colorScheme={"messenger"} marginTop={"5"} variant="outline">
-                Complete project
-              </Button>
-            </>
-          ) : null}
+              {userInfo.role === "ADMIN" ||
+              userInfo.role === "PROJECT_MANAGER" ? (
+                <>
+                  <Button
+                    colorScheme={"messenger"}
+                    marginTop={"5"}
+                    variant="outline"
+                    onClick={onOpen}
+                  >
+                    Complete project
+                  </Button>
+                </>
+              ) : null}
             </>
           ) : (
             "This project is not completed yet"
@@ -34,6 +59,25 @@ function other({ data, de, org }) {
           ) : null}
         </>
       )}
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Alert</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Are sure want to finish this project as completed</ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="messenger" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button colorScheme={"green"} >
+              Yes
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
     </div>
   );
 }
