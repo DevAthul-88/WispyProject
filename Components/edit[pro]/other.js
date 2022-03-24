828da581b050ef0f00829600cec6a552d3ad9356
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-24 19:43:50 +0530

    changes

--- Components/edit[pro]/other.js
+++ Components/edit[pro]/other.js
@@ -1,8 +1,6 @@
 import {
   Heading,
   Button,
-  Stack,
-  Divider,
   Modal,
   ModalBody,
   ModalContent,
@@ -17,27 +15,28 @@ import { useSelector, useDispatch } from "react-redux";
 import axios from "axios";
 import { fetchData } from "../../redux/org/action";
 import { useRouter } from "next/router";
+import DeleteProject from "./delete";
 
 function other({ data, de, org }) {
   const dispatch = useDispatch();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { userInfo } = useSelector((state) => state.auth);
   const [reload, setReload] = React.useState(false);
-  const [loading , setLoading] = React.useState(false)
+  const [loading, setLoading] = React.useState(false);
 
   const router = useRouter();
 
   const handleFinish = async () => {
     setLoading(true);
     const omi = await axios.patch("/api/project/finish", {
-      orgId:org._id,
+      orgId: org._id,
       projectId: router.query.slug,
     });
-    if(omi.data.error) {
+    if (omi.data.error) {
       console.error(omi.data.error);
       setLoading(false);
     }
-    if(omi.data.success) return setReload(true)
+    if (omi.data.success) return setReload(true);
   };
 
   useEffect(() => {
@@ -50,42 +49,39 @@ function other({ data, de, org }) {
     <div>
       {userInfo && (
         <>
-          {de.completed.approved == true  ? <>
-            <Heading fontSize={"xl"}>
-                This project is completed successfully
-              </Heading>
-          </> : <>
-          {de.completed.flagged ? (
+          {de.completed.approved == true ? (
             <>
               <Heading fontSize={"xl"}>
-                This project is flagged as completed
+                This project is completed successfully
               </Heading>
-              {userInfo.role === "ADMIN" ||
-              userInfo.role === "PROJECT_MANAGER" ? (
-                <>
-                  <Button
-                    colorScheme={"messenger"}
-                    marginTop={"5"}
-                    variant="outline"
-                    onClick={onOpen}
-                  >
-                    Complete project
-                  </Button>
-                </>
-              ) : null}
             </>
           ) : (
-            "This project is not completed yet"
-          )}
-          </>}
-          {userInfo.role === "ADMIN" || userInfo.role === "PROJECT_MANAGER" ? (
             <>
-              <Divider marginTop={"5"} />
-              <Button colorScheme={"red"} marginTop={"5"} variant="outline">
-                Delete project
-              </Button>
+              {de.completed.flagged ? (
+                <>
+                  <Heading fontSize={"xl"}>
+                    This project is flagged as completed
+                  </Heading>
+                  {userInfo.role === "ADMIN" ||
+                  userInfo.role === "PROJECT_MANAGER" ? (
+                    <>
+                      <Button
+                        colorScheme={"messenger"}
+                        marginTop={"5"}
+                        variant="outline"
+                        onClick={onOpen}
+                      >
+                        Complete project
+                      </Button>
+                    </>
+                  ) : null}
+                </>
+              ) : (
+                "This project is not completed yet"
+              )}
             </>
-          ) : null}
+          )}
+          <DeleteProject userInfo={userInfo} de={de} org={org} />
         </>
       )}
 
@@ -102,7 +98,11 @@ function other({ data, de, org }) {
             <Button colorScheme="messenger" mr={3} onClick={onClose}>
               Close
             </Button>
-            <Button colorScheme={"green"} onClick={handleFinish} isLoading={loading}>
+            <Button
+              colorScheme={"green"}
+              onClick={handleFinish}
+              isLoading={loading}
+            >
               Yes
             </Button>
           </ModalFooter>
