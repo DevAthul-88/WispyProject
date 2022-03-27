45e2c204b8962e211cf4d46b568dbf3ff7c4e42a
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-27 11:44:53 +0530

    changes

---
+++ pages/api/ticket/index.js
@@ -0,0 +1,88 @@
+import db from "../../../utils/dbConnect";
+import orgModel from "../../../Schema/orgSchema";
+import mongoose from "mongoose";
+
+db();
+
+const objectId = mongoose.Types.ObjectId;
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+        console.log(req.body);
+    //   const { title, description, members, priority, ordId } = req.body;
+    //   const project = {
+    //     _id: new mongoose.Types.ObjectId(),
+    //     title,
+    //     description,
+    //     members,
+    //     todo: new Array(),
+    //     priority,
+    //     tickets:[],
+    //     completed: {
+    //       flagged: false,
+    //       approved: false,
+    //     },
+    //     createdAt: Date.now(),
+    //     updatedAt: Date.now(),
+    //     comments: new Array(),
+    //   };
+
+    //   await orgModel.updateOne(
+    //     { _id: ordId },
+    //     { $push: { projects: project } }
+    //   );
+
+    //   res.send({ refresh: true });
+    } catch (error) {
+      console.log(error.message);
+      res.send({ error: error.message });
+    }
+  } else if (req.method === "PUT") {
+    try {
+      const { title, description, members, priority, ordId, project_id } =
+        req.body;
+
+      await orgModel.findByIdAndUpdate(
+        {
+          _id: ordId,
+        },
+        {
+          $set: {
+            "projects.$[i].title": title,
+            "projects.$[i].description": description,
+            "projects.$[i].members": members,
+            "projects.$[i].priority": priority,
+          },
+        },
+        {
+          arrayFilters: [{ "i._id": objectId(project_id) }],
+        }
+      );
+      res.send({ refresh: true });
+    } catch (error) {
+      res.send({ error: error.message });
+    }
+  } else if (req.method === "PATCH") {
+    try {
+      const { orgId, projectId } = req.body;
+
+      await orgModel.findByIdAndUpdate(
+        {
+          _id: orgId,
+        },
+        {
+          $set: {
+            "projects.$[i].completed.flagged": true,
+          },
+        },
+        {
+          arrayFilters: [{ "i._id": objectId(projectId) }],
+        }
+      );
+      res.send({ reload: true });
+    } catch (error) {
+      res.send({ error: error.message });
+    }
+  }
+}
--- redux/ticket/action.js
+++ redux/ticket/action.js
@@ -3,3 +3,19 @@ import {
   TICKET_CREATE_LOADING,
   TICKET_CREATE_FAILURE,
 } from "./type";
+import axios from "axios";
+
+export const ticketCreateAction = (credentials) => async (dispatch) => {
+  try {
+    dispatch({ type: TICKET_CREATE_LOADING });
+    const { data } = await axios.post("/api/ticket/index", credentials);
+    if (data.error) {
+      return dispatch({ type: TICKET_CREATE_FAILURE, payload: data.error });
+    }
+    if (data.reload) {
+      dispatch({ type: TICKET_CREATE_SUCCESS });
+    }
+  } catch (error) {
+    dispatch({ type: TICKET_CREATE_FAILURE, payload: error.message });
+  }
+};
