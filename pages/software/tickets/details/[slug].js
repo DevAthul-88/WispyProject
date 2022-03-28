889774c9cdfd47491f70e6f8bfae4543ab51d31d
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-28 12:17:20 +0530

    ticket details page

---
+++ pages/software/tickets/details/[slug].js
@@ -0,0 +1,84 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../../../../Components/Loader";
+import { useEffect  , useState} from "react";
+import { fetchData } from "../../../../redux/org/action";
+import {
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
+import Details from '../../../../Components/project.details'
+import Comment from '../../../../Components/Comment/Index'
+import Table from '../../../../Components/Table/users'
+import Todo from '../../../../Components/Todo/Todo'
+import Ticket from '../../../../Components/Ticket.project/index'
+
+function Slug() {
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const { data, loading } = useSelector((state) => state.org);
+  const { userInfo } = useSelector((state) => state.auth);
+  
+  
+  
+  const proj = data && data.projects.filter((e) => e._id == router.query.slug);
+  const users = proj == undefined || proj == null  || proj.length === 0 ? [] : data && data.employees.filter((e) => proj[0].members.includes(e._id))
+  useEffect(() => {
+    dispatch(fetchData(userInfo._id));
+  }, []);
+
+
+  return (
+    <div>
+      <Head>
+        <title>WispyProject - Ticket Details</title>
+      </Head>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {proj == undefined || proj == null  || proj.length === 0 ? (
+            <Loader />
+          ) : (
+            <Container maxWidth={"container.lg"} marginRight={"20"}>
+              <Tabs isFitted variant="enclosed-colored" colorScheme={"messenger"}>
+                <TabList mb="1em">
+                  <Tab>Details</Tab>
+                  <Tab>Tickets</Tab>
+                  <Tab>Comments</Tab>
+                  <Tab>Todos</Tab>
+                  <Tab>Users</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                   <Details data={proj}/>
+                  </TabPanel>
+                  <TabPanel>
+                   <Ticket data={proj}/>
+                  </TabPanel>
+                  <TabPanel>
+                    <Comment comment={proj[0].comments}/>
+                  </TabPanel>
+                  <TabPanel>
+                    <Todo  users={users}/>
+                  </TabPanel>
+                  <TabPanel>
+                    {users && <Table org={users}/>}
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+              
+            </Container>
+          )}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Slug;
