81a8163e6745ca533106e4229a7dd04f6a584d9f
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-03-29 13:34:12 +0530

    changes

--- pages/software/index.js
+++ pages/software/index.js
@@ -1,31 +1,44 @@
 import React from "react";
 import { Container, Flex, Box } from "@chakra-ui/react";
 import Head from "next/head";
-import User from "../../lib/user";
 import { BarChart } from "../../Components/Charts/Bar";
 import { PieChart } from "../../Components/Charts/Pie";
-import Status from '../../Components/status'
+import Status from "../../Components/status";
+import { useSelector } from "react-redux";
+import Loader from "../../Components/Loader";
 
 function index() {
+  const { data, error, loading } = useSelector((state) => state.org);
   return (
     <Container maxWidth={"container.lg"} marginRight={"14"}>
       <Head>
         <title>WispyProject - Dashboard</title>
       </Head>
-      
-      <Status />
-      <Box borderWidth={"thin"} padding={"2"}>
-        <BarChart />
-      </Box>
 
-      <Flex marginTop={"10"}>
-        <Box borderWidth={"thin"} padding={"2"} width={"full"}>
-          <BarChart />
-        </Box>
-        <Box height={"sm"} borderWidth={"thin"} padding={"2"} width={"full"}>
-          <PieChart />
-        </Box>
-      </Flex>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Status />
+          <Box borderWidth={"thin"} padding={"2"}>
+            <BarChart />
+          </Box>
+
+          <Flex marginTop={"10"}>
+            <Box borderWidth={"thin"} padding={"2"} width={"full"}>
+              <BarChart />
+            </Box>
+            <Box
+              height={"sm"}
+              borderWidth={"thin"}
+              padding={"2"}
+              width={"full"}
+            >
+              <PieChart />
+            </Box>
+          </Flex>
+        </>
+      )}
     </Container>
   );
 }
