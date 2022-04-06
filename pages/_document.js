316b6afff4dcc1a50fdfdfa927c78c391c2e921c
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-04-06 16:56:30 +0530

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
+          <meta name="viewport" content="width=device-width" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
           <meta name="theme-color" content="#0078FF" />
         </Head>
